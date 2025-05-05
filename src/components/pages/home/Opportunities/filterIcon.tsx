e43903dbb1d7e276b04c2d9aeab7caec6ac3ed1b
meet <ext.meet@deccan.ai>
2025-05-05 17:24:17 +0530

    filename change - 56

--- src/components/pages/home/Opportunities/filterIcon.tsx
+++ src/components/pages/home/Opportunities/filterIcon.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { IFilterIconType } from "@/interfaces/components/filtericon.type";
+import { IFilterIconType } from "@/interfaces/components/_Filtericon.type";
 import { FC } from "react";
 
 const FilterIcon: FC<IFilterIconType> = ({
