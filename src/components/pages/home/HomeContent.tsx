f2238512d443f28022810f6c70b3f66fb50ec18f
meet <ext.meet@deccan.ai>
2025-05-05 17:06:45 +0530

    same

--- src/components/pages/home/HomeContent.tsx
+++ src/components/pages/home/HomeContent.tsx
@@ -9,7 +9,7 @@ import Statistics from "./Statistics/statistics";
 import Stories from "./stories";
 import NavBar from "../../shared/NavBar";
 import { useEffect, useState } from "react";
-import OpportunitiesAndBenifits from "./_Opportunitiesandbenifits";
+import OpportunitiesAndBenifits from "./Opportunitiesandbenifits";
 
 const HomeContent = () => {
   // Use state to control rendering on the client side
