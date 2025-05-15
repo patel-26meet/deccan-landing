66fe91307c712f15b763bbdcf21e9c306d0b0df9
Mihir K <mihir@deccan.ai>
2025-05-15 18:19:49 +0530

    Add popup functionality with cookie consent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@
 
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
-
+import PopupContainer from '@/components/shared/popup/PopupContainer';
 // Dynamically import the home page content with ssr disabled
 const HomeContent = dynamic(() => import('../components/pages/home/HomeContent'), { ssr: false });
 
 export default function Home() {
   return (
     <Suspense fallback={<div className="loading-fallback">Loading...</div>}>
+      <PopupContainer />
       <HomeContent />
     </Suspense>
   );
---
+++ src/constants/shared/popup.tsx
@@ -0,0 +1,32 @@
+import { TPopupContent, EnumCookieAcceptance } from "@/interfaces/components/popup.type";
+import Cookies from 'js-cookie';
+
+const cookieOptions = {
+  path: "/",
+  domain: process.env.NEXT_PUBLIC_DOMAIN,
+  secure: true,
+  expires: 365
+};
+
+const handleCookieConsent = (isAccepted: boolean) => {
+  const consentValue = isAccepted ? EnumCookieAcceptance.ACCEPT : EnumCookieAcceptance.REJECT;
+  Cookies.set('cookie_consent', consentValue, cookieOptions);
+  console.log(`Cookie ${consentValue}`);
+};
+
+// Helper function to check cookie consent
+export const checkCookieConsent = () => {
+  const cookieValue = Cookies.get('cookie_consent');
+  return cookieValue === EnumCookieAcceptance.ACCEPT;
+};
+
+export const PopupDataConstant: Record<string, TPopupContent> = {
+  cookie: {
+    title: "Cookie Settings ",
+    content: "We use cookies to enhance your experience, analyze site traffic and deliver personalized content. Read our <a class='popup-content__text-link' href='https://soulhq.ai/cookie-policy' target='_blank'>Cookie Policy</a>.",
+    actions: [
+      { label: "Okay", onClick: () => handleCookieConsent(true) },
+    ],
+    onClose: () => handleCookieConsent(true),
+  },
+};
